562f42418546e6e5072acaf0d311df02e9f2bbac
eDD <e6yr@proton.me>
2024-07-05 21:16:25 +0100

    advancing routes

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type taskServer struct {
 	store *TaskStore
+	sync.Mutex
 }
 
 func NewTaskServer() *taskServer {
@@ -18,61 +22,45 @@ func NewTaskServer() *taskServer {
 	return &taskServer{store: store}
 }
 
-func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
-	log.Printf("handling get task at %s\n", req.URL.Path)
-
-	id, err := strconv.Atoi(req.PathValue("id"))
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
 	if err != nil {
-		http.Error(w, "invalid id", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get task at %s\n", req.URL.Path)
+
+	id, _ := strconv.Atoi(mux.Vars(req)["id"])
+
 	task, err := ts.store.GetTask(id)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
 
-	js, err := json.Marshal(task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	renderJSON(w, task)
 }
 
-func (ts *taskServer) dueHandler(w http.ResponseWriter, req http.Request) {
-	year, err := strconv.Atoi(req.PathValue("year"))
-	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return
-	}
-	month, err := strconv.Atoi(req.PathValue("month"))
-	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return
-	}
-	day, err := strconv.Atoi(req.PathValue("day"))
-	if err != nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return
-	}
+func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
+	year, _ := strconv.Atoi(mux.Vars(req)["year"])
+	month, _ := strconv.Atoi(mux.Vars(req)["month"])
+	day, _ := strconv.Atoi(mux.Vars(req)["day"])
+
 	tasks := ts.store.GetTaskByDueDate(year, time.Month(month), day)
 
-	js, err := json.Marshal(tasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	renderJSON(w, tasks)
 }
 
 func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
-	tag := req.PathValue("tag")
+	tag := mux.Vars(req)["tag"]
+
 	if len(tag) == 0 {
 		http.Error(w, "invalid request", http.StatusBadRequest)
 		return
@@ -80,25 +68,20 @@ func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
 
 	tasks := ts.store.GetTaskByTag(tag)
 
-	js, err := json.Marshal(tasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	renderJSON(w, tasks)
 }
+
 func main() {
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
+	router.StrictSlash(true)
 	server := NewTaskServer()
-	mux.HandleFunc("POST /task/", server.CreateTaskHandler)
-	mux.HandleFunc("GET /task/", server.getAllTasksHandler)
-	mux.HandleFunc("DELETE /task/", server.deleteAllTasksHandler)
-	mux.HandleFunc("GET /task/{id}/", server.getTaskHandler)
-	mux.HandleFunc("DELETE /task/{id}/", server.deleteTaskHandler)
-	mux.HandleFunc("GET /tag/{tag}/", server.tagHandler)
-	mux.HandleFunc("GET /due/{year}/{month}/{day}/", server.dueHandler)
+	router.HandleFunc("/task/", server.CreateTaskHandler).Methods("POST")
+	router.HandleFunc("/task/", server.getAllTasksHandler).Methods("GET")
+	router.HandleFunc("/task/", server.deleteAllTasksHandler).Methods("DELETE")
+	router.HandleFunc("/task/{id:[0-9]+}/", server.getTaskHandler).Methods("GET")
+	router.HandleFunc("/task/{id:[0-9]+}/", server.deleteTaskHandler).Methods("DELETE")
+	router.HandleFunc("/tag/{tag}/", server.tagHandler).Methods("GET")
+	router.HandleFunc("/due/{year:[0-9]+}/{month:[0-9]+}/{day:[0-9]+}/", server.dueHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
 }
